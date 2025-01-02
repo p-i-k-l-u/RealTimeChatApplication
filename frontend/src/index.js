91f2535ede16f10affc61bf3d914edf03befe2f8
p-i-k-l-u <piklu19jan@gmail.com>
2025-01-03 01:00:46 +0530

    update code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,7 +11,7 @@ import { persistStore } from 'redux-persist';
 let persistor = persistStore(store);
 
 // export const BASE_URL="http://localhost:8080"
-export const BASE_URL ="real-time-chat-application-twlk.vercel.app"
+export const BASE_URL ="http://real-time-chat-application-twlk.vercel.app"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
